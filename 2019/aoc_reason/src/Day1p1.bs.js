0360dc540b365546b401a11f6bef5d1be94040e2
Matt Ludwigs <mattludwigs@gmail.com>
2019-12-03 08:05:18 -0700

    Refactor some reason code

--- 2019/aoc_reason/src/Day1p1.bs.js
+++ 2019/aoc_reason/src/Day1p1.bs.js
@@ -5,28 +5,28 @@ var Fs = require("fs");
 var List = require("bs-platform/lib/js/list.js");
 var $$Array = require("bs-platform/lib/js/array.js");
 var Js_math = require("bs-platform/lib/js/js_math.js");
-var Caml_format = require("bs-platform/lib/js/caml_format.js");
+var Belt_Int = require("bs-platform/lib/js/belt_Int.js");
+var Belt_Option = require("bs-platform/lib/js/belt_Option.js");
 
 function isNotempty(str) {
   return str !== "";
 }
 
 function calcFuel(mass) {
-  return Js_math.floor(mass / 3.0) - 2 | 0;
+  var match = Belt_Int.fromString(mass);
+  if (match !== undefined) {
+    return Js_math.floor(match / 3.0) - 2 | 0;
+  }
+  
 }
 
-function add(n1, n2) {
-  return n1 + n2 | 0;
+function calcTotalFuel(total, fuel) {
+  return fuel + total | 0;
 }
 
-function calcTotalFuel(total, mass) {
-  return total + calcFuel(mass) | 0;
-}
-
-console.log(List.fold_left(calcTotalFuel, 0, List.map(Caml_format.caml_int_of_string, List.filter(isNotempty)($$Array.to_list(Fs.readFileSync("../inputs/day1.txt", "utf8").split("\n"))))));
+console.log(List.fold_left(calcTotalFuel, 0, List.map(Belt_Option.getExn, List.filter(Belt_Option.isSome)(List.map(calcFuel, List.filter(isNotempty)($$Array.to_list(Fs.readFileSync("../inputs/day1.txt", "utf8").split("\n"))))))));
 
 exports.isNotempty = isNotempty;
 exports.calcFuel = calcFuel;
-exports.add = add;
 exports.calcTotalFuel = calcTotalFuel;
 /*  Not a pure module */
--- 2019/aoc_reason/src/Day1p1.re
+++ 2019/aoc_reason/src/Day1p1.re
@@ -7,18 +7,23 @@ let isNotempty = str =>
     true;
   };
 
-let calcFuel = mass => Js.Math.floor(mass /. 3.0) - 2;
+let calcFuel = mass =>
+  switch (Belt.Int.fromString(mass)) {
+  | None => None /* This should never happen */
+  | Some(i) =>
+    let floatMass = Belt.Int.toFloat(i);
+    Some(Js.Math.floor(floatMass /. 3.0) - 2);
+  };
 
-let add = (n1, n2) => n1 + n2;
-
-let calcTotalFuel = (total, mass) =>
-  mass |> float_of_int |> calcFuel |> add(total);
+let calcTotalFuel = (total, fuel) => fuel + total;
 
 `utf8
 |> Node_fs.readFileSync("../inputs/day1.txt")
 |> Js.String.split("\n")
 |> Array.to_list
 |> List.filter(isNotempty)
-|> List.map(int_of_string)
+|> List.map(calcFuel)
+|> List.filter(Belt.Option.isSome)
+|> List.map(Belt.Option.getExn)
 |> List.fold_left(calcTotalFuel, 0)
 |> Js.log;
